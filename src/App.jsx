006d8c02da375d56f48066894713c4d4a8c52b6e
Ketan <138638658+k10-ketan@users.noreply.github.com>
2025-07-02 00:46:53 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React,{useState} from 'react';
+
+const App = () => {
+  let time = new Date().toLocaleTimeString();
+  const [cTime, setCTime] = useState(time);
+
+  const updateTime = () => {
+    time = new Date().toLocaleTimeString();
+    setCTime(time);
+  }
+  setInterval(updateTime, 1000);
+
+
+
+
+  return (
+    <>
+    <h1>{cTime}</h1>
+    </>
+  )
+}
+export default App;
